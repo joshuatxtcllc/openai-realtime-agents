73a759ebb675fb961ef1d356e38fe9d3914c3e85
joshuatxtcllc <jaysframes@gmail.com>
2025-10-03 12:10:08 -0500

    Fix missing useEffect import

--- src/app/hooks/useHandleSessionHistory.ts
+++ src/app/hooks/useHandleSessionHistory.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { useTranscript } from "@/app/contexts/TranscriptContext";
 import { useEvent } from "@/app/contexts/EventContext";
 
